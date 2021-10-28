97d89cd642721421d407c150c23026cd2e6e4911
Anubhav Sigdel <64096825+asigdel29@users.noreply.github.com>
2021-10-28 18:29:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,30 +3,33 @@ import time
 from gopigo import *
 from easygopigo3 import EasyGoPiGo3
 
+
+print("Welcome")    
+print("Please declare your minimum safe distance")
+safedistance = input()
+##print("Please declare initial speed")
+##current_speed = input()
+
 servo_pos=90
 gpg = EasyGoPiGo3()
-current_speed = 100
 distance_sensor = gpg.init_distance_sensor()
 
-#print("Distance Sensor Reading (mm): " + str(my_distance_sensor.read_mm()))
-
-
-print("W = forward.")
-print("A= left.")
-print("S = backward.")
-print("D = right.")
-print("1 = Servo left")
-print("2 = Servo right")
-print("3 = Servo home")
-print("4 = Increase speed (Default speed = 100)")
-print("5= Decrease speed")
-print("z= Exit")
-
-
 while True:
+    print("W = forward.")
+    print("A= left.")
+    print("S = backward.")
+    print("D = right.")
+    print("1 = Servo left")
+    print("2 = Servo right")
+    print("3 = Servo home")
+    print("4 = Increase speed (Default speed = 100)")
+    print("5= Decrease speed")
+    print("z= Exit")
+
     command = input()
     if command == '4':  #Increase speed
         increase_speed()
+
 
     elif command == '5': #Decrease speed
         decrease_speed()
@@ -46,7 +49,6 @@ while True:
         servo(servo_pos)
         time.sleep(.1)
 
-
     elif command=='w': #Move forward
         fwd()
     elif command=='a': #Turn left
@@ -55,12 +57,15 @@ while True:
         right()
     elif command=='s': #Move back
         bwd()
-
     elif command == 'x': #Stop
         stop()
     elif command == 'z': #Exit
         print("Exiting")
         sys.exit()
+
+    if distance_sensor.read_mm() == 200:
+        print("Obstacle detectd in path: " + str(distance_sensor.read_mm()))
+        stop()
 
     else:
         print("Enter valid command")
