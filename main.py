98927ba3adeb005ffb11ee1fec720d5db7b31fd1
Anubhav Sigdel <64096825+asigdel29@users.noreply.github.com>
2021-11-10 17:31:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,127 +6,96 @@ import colors
 import sys
 from target import *
 from display import draw
-from ultrasonicsensor import ultrasonicRead
-import keyboard
+from gopigo import *
+from collections import Counter
+
+print 'Radar Start'
+
+# initialize the program
+x = pygame.init()
+
+pygame.font.init()
+
+defaultFont = pygame.font.get_default_font()
+
+fontRenderer = pygame.font.Font(defaultFont, 20)
+
+radarDisplay = pygame.display.set_mode((1400, 800))
+    
+pygame.display.set_caption('Radar Screen')
 
 while True:
-    if dist<distance_to_stop:
-        print("Dist:", dist, 'cm')
-        print("Object to Close")
-        print('Stopping')
-        stop()
-    a = input()
-    try:
-
-        if keyboard.is_pressed('w'):
-            x = pygame.init()
-            pygame.font.init()
-            defaultFont = pygame.font.get_default_font()
-            fontRenderer = pygame.font.Font(defaultFont, 20)
-            radarDisplay = pygame.display.set_mode((1400, 800))
-            pygame.display.set_caption('Radar Screen')
-
-            # setup the servo and ultrasonic
-            GPIO.setmode(GPIO.BOARD)
-            GPIO.setwarnings(False)
-
-            servoPin = 12
-            GPIO.setup(servoPin, GPIO.OUT)
-            servo = GPIO.PWM(servoPin, 50)
-            servo.start(7)
-
-            TRIG = 16
-            ECHO = 18
-            GPIO.setup(TRIG, GPIO.OUT)
-            GPIO.setup(ECHO, GPIO.IN)
-
-            # targets list
-            targets = {}
-
-            try:
-                while True:
-
-                    # rotate from 0 to 180
-                    for angle in range(0, 180):
-                        distance = ultrasonicRead(GPIO, TRIG, ECHO)
-
-                    # change the condition if the range is changed
-                        if distance != -1 and distance <= 50:
-                            targets[angle] = Target(angle, distance)
-                            draw(radarDisplay, targets, angle, distance, fontRenderer)
-                            angle = 180 - angle
-                            dc = 1.0 / 18.0 * angle + 2
-                            servo.ChangeDutyCycle(dc)
-                            time.sleep(0.001)
-
-                    # rotate from 180 to 0
-                    for angle in range(180, 0, -1):
-                        distance = ultrasonicRead(GPIO, TRIG, ECHO)
-
-                    # change the condition if the range is changed
-                        if distance != -1 and distance <= 50:
-                            targets[angle] = Target(angle, distance)
-
-                        draw(radarDisplay, targets, angle, distance, fontRenderer)
-
-                        angle = 180 - angle
-                        dc = 1.0 / 18.0 * angle + 2
-                        servo.ChangeDutyCycle(dc)
-
-                        time.sleep(0.001)
-
-                    # detect if close is pressed to stop the program
-                    for event in pygame.event.get():
-                        if event.type == pygame.QUIT:
-                            raise KeyboardInterrupt
-
-            except KeyboardInterrupt:
-                servo.stop()
-                GPIO.cleanup()
-
-            except Exception as e:
-                servo.stop()
-                GPIO.cleanup()
-
-            fwd()
-            time.sleep(.1)
-
-        if keyboard.is_pressed('a'):
-            left()
-
-        if keyboard.is_pressed('d'):
-            right()
-
-        if keyboard.is_pressed('s'):
-            bwd()
-
-        if keyboard.is_pressed('x'):
-            stop()
-
-        if keyboard.is_pressed('t'):
-            increase_speed()
-
-        if keyboard.is_pressed('g'):
-            decrease_speed()
-        elif a == '4':
-            servo_pos = servo_pos + 10
-
-        elif a == '6':
-            servo_pos = servo_pos - 50
-
-        elif a == '0':
-            servo_pos = 90
-
-        if servo_pos > 90:
-            servo_pos = 180
-        if servo_pos < 0:
-            servo_pos = 180
-
-        servo(servo_pos)
-        time.sleep(.1)
-
-    except:
+    delay=.02
+    debug =0                    #True to print all raw values
+    num_of_readings=45          #Number of readings to take 
+    incr=180/num_of_readings    #increment of ang in servo
+    ang_l=[0]*(num_of_readings+1)   #list to hold the ang's of readings
+    dist_l=[0]*(num_of_readings+1)  #list to hold the distance at each ang
+    x=[0]*(num_of_readings+1)   #list to hold the x coordinate of each point
+    y=[0]*(num_of_readings+1)   #list to hold the y coordinate of each point
+    buf=[0]*40
+    ang=0
+    lim=250     #maximum limit of distance measurement (any value over this which be initialized to the limit value)
+    index=0
+    sample=2
+    targets = {}
+    enable_servo()
+    enable_com_timeout(2000)
+    enc_tgt(1,1,18) #Set encoder targetting. Stop after 4 rotations of both the wheels
+    
+    for i in range(sample): 
+        dist=us_dist(15)
+        if dist<lim and dist>=0:
+            buf[i]=dist
+        else:
+            buf[i]=lim
+            
+    max=Counter(buf).most_common()  
+    rm=-1
+    
+    for i in range (len(max)):
+        if max[i][0] <> lim and max[i][0] <> 0:
+            rm=max[i][0]
+            break
+    if rm==-1:
+        rm=lim
+        
+    if debug==1:
+        print index,ang,rm
+    ang_l[index]=ang
+    dist_l[index]=rm
+    index+=1
+            
+    servo(ang)  
+    time.sleep(delay)
+    ang+=incr
+            
+    #print ang
+    if ang>90:
         break
+        
+        #if dist != -1 and dist <= 50:
+    targets[ang] = Target(ang, dist)      
+    draw(radarDisplay, targets, ang, dist, fontRenderer)
 
+        #ang = 180 - ang
+        #dc = 1.0 / 18.0 * ang + 2
+        #servo.ChangeDutyCycle(dc)
+    time.sleep(0.001)
+        
+        
+            # change the condition if the range is changed
+            #if distance != -1 and distance <= 50:
+             #   targets[ang] = Target(ang, distance)
+            
+        #draw(radarDisplay, targets, ang, dist, fontRenderer)
+
+    
+        # detect if close is pressed to stop the program
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        raise KeyboardInterrupt
+            
+disable_servo()
 pygame.quit()
 sys.exit()
