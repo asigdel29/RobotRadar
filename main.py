aaa913f61f876b0bfc83cc0ed743806392fa1d30
Anubhav Sigdel <64096825+asigdel29@users.noreply.github.com>
2021-11-11 16:45:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ radarDisplay = pygame.display.set_mode((1400, 800))
     
 pygame.display.set_caption('Radar Screen')
 
-while True:
+
+def us_map():
     delay=.02
     debug =0                    #True to print all raw values
     num_of_readings=45          #Number of readings to take 
@@ -40,61 +41,68 @@ while True:
     sample=2
     targets = {}
     enable_servo()
-    enable_com_timeout(2000)
-    enc_tgt(1,1,18) #Set encoder targetting. Stop after 4 rotations of both the wheels
-    
-    for i in range(sample): 
-        dist=us_dist(15)
-        if dist<lim and dist>=0:
-            buf[i]=dist
-        else:
-            buf[i]=lim
+    servo_pos=180
+
+    while True:
+        for i in range(sample): 
+            dist=us_dist(15)
+            if dist<lim and dist>=0:
+                buf[i]=dist
+            else:
+                buf[i]=lim
             
-    max=Counter(buf).most_common()  
-    rm=-1
-    
-    for i in range (len(max)):
-        if max[i][0] <> lim and max[i][0] <> 0:
-            rm=max[i][0]
+        max=Counter(buf).most_common()  
+        rm=-1
+        for i in range (len(max)):
+            if max[i][0] <> lim and max[i][0] <> 0:
+                rm=max[i][0]
             break
-    if rm==-1:
-        rm=lim
+        if rm==-1:
+            rm=lim
         
-    if debug==1:
-        print index,ang,rm
-    ang_l[index]=ang
-    dist_l[index]=rm
-    index+=1
+        if debug==1:
+            print index,ang,rm
+        ang_l[index]=ang
+        dist_l[index]=rm
+        index+=1
             
-    servo(ang)  
-    time.sleep(delay)
-    ang+=incr
+        servo(ang)  
+        time.sleep(delay)
+        ang+=incr
             
-    #print ang
-    if ang>90:
-        break
+        #print ang
+        #if ang>90:
+        #    break
         
-        #if dist != -1 and dist <= 50:
-    targets[ang] = Target(ang, dist)      
-    draw(radarDisplay, targets, ang, dist, fontRenderer)
+        if dist != -1 and dist <= 50:
+            targets[ang] = Target(ang, dist)      
+    
+        draw(radarDisplay, targets, ang, dist, fontRenderer)
 
         #ang = 180 - ang
         #dc = 1.0 / 18.0 * ang + 2
         #servo.ChangeDutyCycle(dc)
-    time.sleep(0.001)
-        
-        
-            # change the condition if the range is changed
-            #if distance != -1 and distance <= 50:
-             #   targets[ang] = Target(ang, distance)
-            
-        #draw(radarDisplay, targets, ang, dist, fontRenderer)
+        time.sleep(0.001)
 
-    
+while True:
+    enable_com_timeout(2000)
+    enc_tgt(1,1,18) #Set encoder targetting. Stop after 4 rotations of both the wheels
+    time.sleep(.2)
+    while True:
+        enc=read_enc_status()
+        ts=read_timeout_status()
+        time.sleep(.05)
+        if enc == 0:    #Stop when target is reached
+            break
+        if  ts==0:
+            break
+    if us_map() <20:    #If any obstacle is closer than 20 cm, stop
+        break
+    disable_servo()
         # detect if close is pressed to stop the program
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        raise KeyboardInterrupt
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            raise KeyboardInterrupt
             
 disable_servo()
 pygame.quit()
