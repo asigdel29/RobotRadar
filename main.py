7656484f0ba1e7431d29a732240e563415c7e4b9
Anubhav Sigdel <64096825+asigdel29@users.noreply.github.com>
2021-11-14 19:40:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,109 +1,63 @@
-import RPi.GPIO as GPIO
-import pygame
-import math
-import time
-import colors
-import sys
-from target import *
-from display import draw
 from gopigo import *
-from collections import Counter
 
-print 'Radar Start'
+print ("CONTROLS")
+print ("q: Radar Scan")
+print ("w: move robot forward")
+print ("a: move robot left")
+print ("s: move robot backward")
+print ("d: move robot right")
+print ("z: stop robot")
+print ("4: move servo left")
+print ("6: move servo right")
+print ("5: move servo home")
+print ("1: Increase speed")
+print ("2: Speed decreases")
+print ("Exit to Terminate")
+print ("Press ENTER to send the commands")
 
-# initialize the program
-x = pygame.init()
 
-pygame.font.init()
-
-defaultFont = pygame.font.get_default_font()
-
-fontRenderer = pygame.font.Font(defaultFont, 20)
-
-radarDisplay = pygame.display.set_mode((1400, 800))
-    
-pygame.display.set_caption('Radar Screen')
-
-
-def us_map():
-    delay=.02
-    debug =0                    #True to print all raw values
-    num_of_readings=45          #Number of readings to take 
-    incr=180/num_of_readings    #increment of ang in servo
-    ang_l=[0]*(num_of_readings+1)   #list to hold the ang's of readings
-    dist_l=[0]*(num_of_readings+1)  #list to hold the distance at each ang
-    x=[0]*(num_of_readings+1)   #list to hold the x coordinate of each point
-    y=[0]*(num_of_readings+1)   #list to hold the y coordinate of each point
-    buf=[0]*40
-    ang=0
-    lim=250     #maximum limit of distance measurement (any value over this which be initialized to the limit value)
-    index=0
-    sample=2
-    targets = {}
-    enable_servo()
-    servo_pos=180
-
-    while True:
-        for i in range(sample): 
-            dist=us_dist(15)
-            if dist<lim and dist>=0:
-                buf[i]=dist
-            else:
-                buf[i]=lim
-            
-        max=Counter(buf).most_common()  
-        rm=-1
-        for i in range (len(max)):
-            if max[i][0] <> lim and max[i][0] <> 0:
-                rm=max[i][0]
-            break
-        if rm==-1:
-            rm=lim
-        
-        if debug==1:
-            print index,ang,rm
-        ang_l[index]=ang
-        dist_l[index]=rm
-        index+=1
-            
-        servo(ang)  
-        time.sleep(delay)
-        ang+=incr
-            
-        #print ang
-        #if ang>90:
-        #    break
-        
-        if dist != -1 and dist <= 50:
-            targets[ang] = Target(ang, dist)      
-    
-        draw(radarDisplay, targets, ang, dist, fontRenderer)
-
-        #ang = 180 - ang
-        #dc = 1.0 / 18.0 * ang + 2
-        #servo.ChangeDutyCycle(dc)
-        time.sleep(0.001)
-
+distance_to_stop=20 
 while True:
-    enable_com_timeout(2000)
-    enc_tgt(1,1,18) #Set encoder targetting. Stop after 4 rotations of both the wheels
-    time.sleep(.2)
-    while True:
-        enc=read_enc_status()
-        ts=read_timeout_status()
-        time.sleep(.05)
-        if enc == 0:    #Stop when target is reached
-            break
-        if  ts==0:
-            break
-    if us_map() <20:    #If any obstacle is closer than 20 cm, stop
-        break
-    disable_servo()
-        # detect if close is pressed to stop the program
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            raise KeyboardInterrupt
-            
-disable_servo()
-pygame.quit()
-sys.exit()
+    inp=raw_input()
+    if inp=='w':
+        fwd()
+        while True:
+            dist=us_dist(15)
+            if dist<distance_to_stop:   
+                print "Object too close"
+                print "Stopping"
+                stop()
+                time.sleep(.1)
+    elif inp=='a':
+        left()  
+    elif inp=='d':
+        right() 
+    elif inp=='s':
+        bwd()   
+    elif inp=='x':
+        stop()  
+    elif inp=='1':
+        increase_speed()    # Increase speed
+    elif inp=='2':
+        decrease_speed()    # Decrease speed
+    elif inp=='z':
+        stop()
+    elif inp =='exit':
+        print ("Exiting")     # Exit
+        sys.exit()             
+    elif inp=='4':
+        servo_pos=servo_pos+10
+    elif inp=='6':
+        servo_pos=servo_pos-10
+    elif inp=='5':
+        servo_pos=90
+    elif inp =='q':
+        from radar import *
+   
+    if servo_pos>180:
+        servo_pos=180
+    if servo_pos<0:
+        servo_pos=0
+        
+    servo(servo_pos)        
+    time.sleep(.1)  
