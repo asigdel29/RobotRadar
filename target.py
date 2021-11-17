3b0c730d33dedda500e5cfddf574a7fe2f2311b4
Anubhav Sigdel <64096825+asigdel29@users.noreply.github.com>
2021-11-17 20:15:03 +0000

    Update target.py

--- target.py
+++ target.py
@@ -1,11 +1,13 @@
 import time
 import colors
+from gopigo import *
 
 class Target:
     angle = -1
     distance = -1
     time = -1.0
     color = ()
+    
 
     # initalization
     def __init__(self, angle, distance):
@@ -13,3 +15,4 @@ class Target:
         self.distance = distance
         self.time = time.time()
         self.color = colors.red
+        
