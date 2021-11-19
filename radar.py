9465de1e17009d7fc528c6526ee39c4a6f5a1f7c
Anubhav Sigdel <64096825+asigdel29@users.noreply.github.com>
2021-11-19 02:49:37 +0000

    Update radar.py

--- radar.py
+++ radar.py
@@ -4,7 +4,7 @@ import time
 import colors
 import sys
 from target import *
-from display import draw
+from display import *
 from gopigo import *
 from collections import Counter
 
@@ -33,13 +33,17 @@ def us_map():
     x=[0]*(num_of_readings+1)   #list to hold the x coordinate of each point
     y=[0]*(num_of_readings+1)   #list to hold the y coordinate of each point
     buf=[0]*40
-    ang=170
+    ang=180
     lim=250     #maximum limit of distance measurement (any value over this which be initialized to the limit value)
     index=0
     sample=1
     targets = {}
     enable_servo()
-    servo_pos=180
+    servo_pos=160
+    running = 1
+    turnTime = .305
+    driveTime = .305
+    j = 0
 
     while True:
         for i in range(sample): 
@@ -60,37 +64,98 @@ def us_map():
         
         if debug==1:
             print index,ang,rm
+        
+        if running == 1:
+            servo(ang)  
+            time.sleep(delay)
+            ang-=incr
             
-        servo(ang)  
-        time.sleep(delay)
-        ang-=incr
-        
-        
-        if dist != -1 and dist <= 50:
-            targets[ang] = Target(ang, dist)      
-    
-        draw(radarDisplay, targets, ang, dist, fontRenderer)
 
-        time.sleep(0.001)
-        grid_size=51
-        if ang<10:
-            time.sleep(.5)
-            #Convert the distance and angle to (x,y) coordinates and scale it down
-            for i in range(num_of_readings+1):  
-                x[i]=(int(dist_l[i]*math.cos(math.pi*(ang_l[i])/180))/10)
-                y[i]=int(dist_l[i]*math.sin(math.pi*ang_l[i]/180))/10
-            #Rotate the readings so that it is printed in the correct manner
-            for i in range(num_of_readings+1):  
-                x[i]=(grid_size/2)-x[i]
-                y[i]=(grid_size/2)-y[i]
-            #Create a grid
-            grid = [[0 for a in xrange(grid_size)] for a in xrange(grid_size)] 
-            for i in range (num_of_readings+1):
-                if dist_l[i]<>lim:
-                    grid[y[i]][x[i]]=1  
-            fence='-'*(grid_size+1)
-            ang =170
-
+            if dist != -1 and dist <= 50:
+                targets[ang] = Target(ang, dist)
+            
+            draw(radarDisplay, targets, ang, dist, fontRenderer)
+        
+            if ang<25:
+                ang =180
+                servo(90)
+                running = 0
+                
+        for ev in pygame.event.get():      
+                if ev.type == pygame.KEYDOWN:
+                    if ev.key == pygame.K_RIGHT:
+                        enable_com_timeout(2000)
+                        enc_tgt(0,1,8) #Set encoder targetting. Stop after 4 rotations of both the wheels
+                        right()
+                        time.sleep(2)   
+                    if ev.key == pygame.K_LEFT:
+                        left()
+                        time.sleep(turnTime)
+                        for angle in targets.keys():
+                            if targets[angle].angle > 30:
+                                targets[angle].angle = targets[angle].angle - 30
+                                print targets[angle].angle
+                            elif targets[angle].angle <= 30:
+                                targets[angle].angle = targets[angle].angle + 180
+                                print targets[angle].angle
+                                
+                    if ev.key == pygame.K_UP:
+                        fwd()
+                        time.sleep(driveTime)
+                        
+                    if ev.key == pygame.K_DOWN:
+                        bwd()
+                        time.sleep(driveTime)
+                        
+                    if ev.key == pygame.K_1:
+                        exit()
+                    if ev.key == pygame.K_0:
+                        running = 1
+                        
+                    if ev.key == pygame.K_2:
+                        pygame.display.update()
+                        
+                if ev.type == pygame.KEYUP:
+                    if ev.key == pygame.K_LEFT or ev.key == pygame.K_RIGHT or ev.key == pygame.K_UP or ev.key == pygame.K_DOWN:
+                        stop()
+                        
+                        
+  
+#            for event in pygame.event.get():
+#                if event.type == pygame.QUIT: 
+#                    sys.exit()        
+#                elif event.type == pygame.KEYDOWN:          # check for key presses          
+#                    if event.key == pygame.K_LEFT:        # left arrow turns left
+#                        pressed_left = True
+#                    elif event.key == pygame.K_RIGHT:     # right arrow turns right
+#                        pressed_right = True
+#                    elif event.key == pygame.K_UP:        # up arrow goes up
+#                        pressed_up = True
+#                    elif event.key == pygame.K_DOWN:     # down arrow goes down
+#                        pressed_down = True
+#                elif event.type == pygame.KEYUP:            # check for key releases
+#                    if event.key == pygame.K_LEFT:        # left arrow turns left
+#                        pressed_left = False
+#                    elif event.key == pygame.K_RIGHT:     # right arrow turns right
+#                        pressed_right = False
+#                    elif event.key == pygame.K_UP:        # up arrow goes up
+#                        pressed_up = False
+#                    elif event.key == pygame.K_DOWN:     # down arrow goes down
+#                        pressed_down = False
+#
+## In your game loop, check for key states:
+#                if pressed_left:
+#                    enc_tgt(1,0,32)
+#                    left()
+#    
+#                if pressed_right:
+#                    x += x_speed
+#if pressed_up:
+#    y -= y_speed
+#if pressed_down:
+#    y += y_speed
+#          
+        
 while True:
     enable_com_timeout(2000)
     enc_tgt(1,1,18) #Set encoder targetting. Stop after 4 rotations of both the wheels
