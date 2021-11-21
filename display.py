603c9af1ddde7edb282e55294b1f2d2dfe648a90
Anubhav Sigdel <64096825+asigdel29@users.noreply.github.com>
2021-11-21 21:01:36 +0000

    Update display.py
    Scaled UI

--- display.py
+++ display.py
@@ -1,156 +1,123 @@
+import colors
 import pygame
 import math
 import time
-import colors
-import sys
-from target import *
-from display import *
 from gopigo import *
+import sys
 from collections import Counter
+distance_to_stop=20
 
-
-# initialize the program
-x = pygame.init()
-
-pygame.font.init()
-
-defaultFont = pygame.font.get_default_font()
-
-fontRenderer = pygame.font.Font(defaultFont, 20)
-
-radarDisplay = pygame.display.set_mode((1400, 800))
+def draw(radarDisplay, targets, angle, distance, fontRenderer):
+     # draw initial screen
+    radarDisplay.fill(colors.black)
     
-pygame.display.set_caption('Radar Screen')
+    pygame.draw.circle(radarDisplay, colors.green, (630,700), 600, 3)
+
+    pygame.draw.circle(radarDisplay, colors.green, (630,700), 500, 3)
+
+    pygame.draw.circle(radarDisplay, colors.green, (630,700), 400, 3)
+
+    pygame.draw.circle(radarDisplay, colors.green, (630,700), 300, 3)
+
+    pygame.draw.circle(radarDisplay, colors.green, (630,700), 200, 3)
+
+    pygame.draw.circle(radarDisplay, colors.green, (630,700), 100, 3)
+
+    radarDisplay.fill(colors.black, [0, 785, 1400, 20])
 
 
-def us_map():
-    delay=.02
-    debug =0                    #True to print all raw values
-    num_of_readings=45          #Number of readings to take 
-    incr=180/num_of_readings    #increment of ang in servo
-    ang_l=[0]*(num_of_readings+1)   #list to hold the ang's of readings
-    dist_l=[0]*(num_of_readings+1)  #list to hold the distance at each ang
-    x=[0]*(num_of_readings+1)   #list to hold the x coordinate of each point
-    y=[0]*(num_of_readings+1)   #list to hold the y coordinate of each point
-    buf=[0]*40
-    ang=25
-    lim=1000     #maximum limit of distance measurement (any value over this which be initialized to the limit value)
-    index=0
-    sample=1
-    targets = {}
-    enable_servo()
-    servo_pos=160
-    running = 1
-    turnTime = .305
-    driveTime = .305
-    j = 0
+    # 45 degree line
+    pygame.draw.line(radarDisplay, colors.green, (630, 750),(205, 285), 3)
 
-    while True:
-        for i in range(sample): 
-            dist=us_dist(15)
-            if dist<lim and dist>=0:
-                buf[i]=dist
-            else:
-                buf[i]=lim
-            
-        max=Counter(buf).most_common()  
-        rm=-1
-        for i in range (len(max)):
-            if max[i][0] <> lim and max[i][0] <> 0:
-                rm=max[i][0]
-            break
-        if rm==-1:
-            rm=lim
-        
-        if debug==1:
-            print index,ang,rm
-        
-        if running == 1:
-            servo(ang)  
-            time.sleep(delay)
-            ang+=incr
-            
+    # 90 degree line
+    pygame.draw.line(radarDisplay, colors.green, (630, 750), (650, 115), 3)
 
-            if dist != -1 and dist <= 50:
-                targets[ang] = Target(ang, dist)
-            
-            draw(radarDisplay, targets, ang, dist, fontRenderer)
-        
-            if ang>170:
-                ang =25
-                servo(90)
-                running = 0
-                
-        for ev in pygame.event.get():      
-                if ev.type == pygame.KEYDOWN:
-                    if ev.key == pygame.K_RIGHT:
-                        right()
-                        time.sleep(turnTime)
-                        for angle in targets.keys():
-                            if targets[angle].angle >= 330:
-                                targets[angle].angle = targets[angle].angle - 330
-                                print targets[angle].angle
-                            elif targets[angle].angle < 0:
-                                targets[angle].angle = targets[angle].angle + 330
-                                print targets[angle].angle   
-                    if ev.key == pygame.K_LEFT:
-                        left()
-                        time.sleep(turnTime)
-                        for angle in targets.keys():
-                            if targets[angle].angle >= 30:
-                                targets[angle].angle = targets[angle].angle - 30
-                                print targets[angle].angle
-                            elif targets[angle].angle < 30:
-                                targets[angle].angle = targets[angle].angle + 330
-                                print targets[angle].angle
-                                
-                    if ev.key == pygame.K_UP:
-                        fwd()
-                        time.sleep(driveTime)
-                        for angle in targets.keys():
-                                targets[angle].distance = targets[angle].distance - 10
-                        
-                    if ev.key == pygame.K_DOWN:
-                        bwd()
-                        time.sleep(driveTime)
-                        for angle in targets.keys():
-                                targets[angle].distance = targets[angle].distance + 10
+    # 135 degree line
+    pygame.draw.line(radarDisplay, colors.green, (630, 750), (1090, 285), 3)
 
-                        
-                    if ev.key == pygame.K_1:
-                        exit()
-                    if ev.key == pygame.K_0:
-                        running = 1
-                        
-                    if ev.key == pygame.K_2:
-                        for targets in targets.keys():
-                            del targets
-                        
-                if ev.type == pygame.KEYUP:
-                    if ev.key == pygame.K_LEFT or ev.key == pygame.K_RIGHT or ev.key == pygame.K_UP or ev.key == pygame.K_DOWN:
-                        stop()   
-        
-while True:
-    enable_com_timeout(2000)
-    enc_tgt(1,1,18) #Set encoder targetting. Stop after 4 rotations of both the wheels
-    time.sleep(.2)
-    while True:
-        enc=read_enc_status()
-        ts=read_timeout_status()
-        time.sleep(.05)
-        if enc == 0:    #Stop when target is reached
-            break
-        if  ts==0:
-            break
-    if us_map() <20:    #If any obstacle is closer than 20 cm, stop
-        break
+    # draw stastics board
+    pygame.draw.rect(radarDisplay, colors.blue, [30, 30, 250, 160], 2)
+
+    # write the 0 degree
+    text = fontRenderer.render("0", 1, colors.green)
+    radarDisplay.blit(text,(10,680))
+
+    #write the 45 degree
+    text = fontRenderer.render("45", 1, colors.green)
+    radarDisplay.blit(text,(175,270))
+
+    # write the 90 degree
+    text = fontRenderer.render("90", 1, colors.green)
+    radarDisplay.blit(text,(640,80))
+
+    # write the 135 degree
+    text = fontRenderer.render("135", 1, colors.green)
+    radarDisplay.blit(text,(1100,285))
+
+    # write the 180 degree
+    text = fontRenderer.render("180", 1, colors.green)
+    radarDisplay.blit(text,(1230,680))
+
+    # draw the moving line
+    a = math.sin(math.radians(angle)) * 700.0
+    b = math.cos(math.radians(angle)) * 700.0
+    pygame.draw.line(radarDisplay, colors.green, (700, 780), (700 - int(b), 780 - int(a)), 3)
+
+    # write the current angle
+    #text = fontRenderer.render("Angle : " + str(angle), 1, colors.white)
+    if angle > 170:
+        text = fontRenderer.render("Angle : 90 " , 1, colors.white)
+    else:
+        text = fontRenderer.render("Angle : " + str(angle), 1, colors.white)
+    radarDisplay.blit(text,(40,40))
     
-    disable_servo()
-        # detect if close is pressed to stop the program
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            raise KeyboardInterrupt
-            
-disable_servo()
-pygame.quit()
-sys.exit()
+    text = fontRenderer.render("Forward : " + str('Up Arrow'), 1, colors.white)
+    radarDisplay.blit(text, (40,60))
+    
+
+    text = fontRenderer.render("Backward : " + str('Down Arrow'), 1, colors.white)    
+    radarDisplay.blit(text, (40,80))
+    
+    
+    text = fontRenderer.render("Left : " + str('Left Arrow'), 1, colors.white)
+    radarDisplay.blit(text, (40,100))
+    
+    text = fontRenderer.render("Right : " + str('Right Arrow'), 1, colors.white)     
+    radarDisplay.blit(text, (40,120))
+    
+    text = fontRenderer.render("Radar Sweep : " + str('0'), 1, colors.white)
+    radarDisplay.blit(text, (40,140))
+    
+    
+    text = fontRenderer.render("Exit : " + str('1'), 1, colors.white)
+    radarDisplay.blit(text, (40,160))
+
+
+    # draw targets
+    for angle in targets.keys():
+        # calculate the coordinates and the remoteness of the target
+        c = math.sin(math.radians(targets[angle].angle)) * 800.0
+        d = math.cos(math.radians(targets[angle].angle)) * 800.0
+        # change the scale if the range is changed
+        e = math.sin(math.radians(targets[angle].angle)) * (700 / 50) * targets[angle].distance
+        f = math.cos(math.radians(targets[angle].angle)) * (700 / 50) * targets[angle].distance
+        
+
+        if targets[angle].distance < 15:
+            color = colors.red
+        elif 35 >= targets[angle].distance >= 15:
+            color = colors.orange
+        elif targets[angle].distance > 35:
+            color = colors.green
+        
+
+        # draw the line indicating the target
+        pygame.draw.circle(radarDisplay, color, (700 - int(f), 780 - int(e)), 20)
+        
+        #pygame.draw.line(radarDisplay, targets[angle].color, (700 - int(f), 780 - int(e)), (700 - int(d), 780 - int(c)), 3)
+
+    
+
+
+    # update the screen
+    pygame.display.update()
